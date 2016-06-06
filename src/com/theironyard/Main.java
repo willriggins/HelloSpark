68165a9f67a8791272d0c24b30e54bce7680a986
willriggins <williamriggins2013@gmail.com>
2016-06-06 11:44:45 -0400

    initial import

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,40 @@
+package com.theironyard;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
+public class Main {
+
+    static User user;
+
+    public static void main(String[] args) {
+	// write your code here
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(m, "login.html");
+                    }
+                    else {
+                        m.put("name", user.name);
+                        return new ModelAndView(m, "home.html");
+                    }
+                },
+               new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                  String username = request.queryParams("username");
+                    user = new User(username);
+                    response.redirect("/");
+                    return "";
+                }
+        );
+    }
+}
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by will on 6/6/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
