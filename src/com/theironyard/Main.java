75e51396d820c66eb0854036c7f8023b5bf8a00e
willriggins <williamriggins2013@gmail.com>
2016-06-06 12:04:52 -0400

    user history

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -4,11 +4,13 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     static User user;
+    static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args) {
 	// write your code here
@@ -22,6 +24,7 @@ public class Main {
                     }
                     else {
                         m.put("name", user.name);
+                        m.put("users", userList);
                         return new ModelAndView(m, "home.html");
                     }
                 },
@@ -32,6 +35,7 @@ public class Main {
                 (request, response) -> {
                   String username = request.queryParams("username");
                     user = new User(username);
+                    userList.add(user);
                     response.redirect("/");
                     return "";
                 }
