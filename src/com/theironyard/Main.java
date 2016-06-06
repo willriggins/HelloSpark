5c369015a7a75b159de5a9a3ba15da752785dadf
willriggins <williamriggins2013@gmail.com>
2016-06-06 11:54:37 -0400

    logout functionality

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -36,5 +36,13 @@ public class Main {
                     return "";
                 }
         );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    user = null;
+                    response.redirect("/");
+                    return "";
+                }
+        );
     }
 }
